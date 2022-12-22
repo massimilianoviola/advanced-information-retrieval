76a2e9a2df00a842a7d9d92650e86dc8c3606d99
Massimiliano Viola <68594973+massimilianoviola@users.noreply.github.com>
2022-12-22 19:56:05 +0100

    preprocessing.py for cacm

---
+++ preprocessing.py
@@ -0,0 +1,80 @@
+import json
+import os
+
+
+######### CONFIG #########
+
+cacm_output_path = "data/cacm"
+os.makedirs(cacm_output_path, exist_ok=True)
+
+
+######### CACM #########
+
+print("Preprocessing CACM...")
+
+with open("cacm/cacm.all") as f, open(os.path.join(cacm_output_path, "cacm.json"), "w") as g:
+    lines = ""
+    for l in f.readlines():
+        lines += "\n" + l.strip() if l.startswith(".") else " " + l.strip()
+    lines = lines.lstrip("\n").split("\n")
+    
+    count = 0
+    doc_dict = {}
+    for l in lines:
+        # id
+        if l.startswith(".I"):
+            doc_id = l.split(" ")[1].strip()
+            doc_dict["DOCID"] = doc_id
+        # title
+        elif l.startswith(".T"):
+            title = l.strip()[3:]
+            doc_dict["TITLE"] = title
+        # abstract
+        elif l.startswith(".W"):
+            abstract = l.strip()[3:]
+            doc_dict["ABSTRACT"] = abstract
+        # last attribute (a doc is a sequence of lines .I, .T, .W, .B, .A, .N, .X)
+        elif l.startswith(".X"):
+            json.dump(doc_dict, g) # write doc record to file
+            g.write("\n")
+            count += 1
+            
+            doc_dict = {} # reset record
+    
+    print(f"Wrote {count} records to disk")
+
+
+with open("cacm/query.text") as f, open(os.path.join(cacm_output_path, "queries.json"), "w") as g:
+    lines = ""
+    for l in f.readlines():
+        lines += "\n" + l.strip() if l.startswith(".") else " " + l.strip()
+    lines = lines.lstrip("\n").split("\n")
+    
+    queries = []
+    query_dict = {}
+    for l in lines:
+        # id
+        if l.startswith(".I"):
+            query_id = l.split(" ")[1].strip()
+            query_dict["QUERYID"] = query_id
+        # query text and last useful attribute
+        elif l.startswith(".W"):
+            query = l.strip()[3:]
+            query_dict["QUERY"] = query
+            
+            queries.append(query_dict) # add record to queries list
+            query_dict = {} # reset record
+    
+    # write queries list to file
+    dictionary = {"QUERIES": queries}
+    json.dump(dictionary, g)
+    
+    print(f"Wrote {len(queries)} queries to disk")
+
+
+with open("cacm/qrels.text") as f, open(os.path.join(cacm_output_path, "qrels-treceval.txt"), "w") as g:
+    lines = f.readlines()
+    for l in lines:
+        query_id, doc_ic, _, _ = l.split()
+        # write in trec_eval format
+        g.write(f"{query_id} Q0 {doc_ic} 1\n")

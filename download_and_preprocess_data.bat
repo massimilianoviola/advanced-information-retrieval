227a940564d41c03bd65060e853aa0d9022243ea
NicoOhler <32089075+NicoOhler@users.noreply.github.com>
2022-12-29 12:24:27 +0100

    Add preprocessing batch file for windows

---
+++ download_and_preprocess_data.bat
@@ -0,0 +1,27 @@
+::mkdir download
+cd download
+mkdir med
+mkdir cacm
+mkdir npl
+
+:: download Medline
+curl -O http://ir.dcs.gla.ac.uk/resources/test_collections/medl/med.tar.gz
+tar -zxf med.tar.gz -C med/
+rm med.tar.gz
+
+:: download CACM
+curl -O http://ir.dcs.gla.ac.uk/resources/test_collections/cacm/cacm.tar.gz
+tar -zxf cacm.tar.gz -C cacm/
+rm cacm.tar.gz
+
+:: download NPL
+curl -O http://ir.dcs.gla.ac.uk/resources/test_collections/npl/npl.tar.gz
+tar -zxf npl.tar.gz -C npl/
+rm npl.tar.gz
+pause 
+:: convert datasets to JSON format
+cd ..
+python3 ./preprocessing.py
+
+:: clean
+rm -rf download/

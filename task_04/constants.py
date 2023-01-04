ad98200d7d8252007a019cc57ab6a49948d951de
NicoOhler <32089075+NicoOhler@users.noreply.github.com>
2023-01-04 21:54:27 +0100

    Fine tune model before creating embeddings

--- task_04/constants.py
+++ task_04/constants.py
@@ -8,7 +8,7 @@ LANGUAGES = ["EN", "DE"]
 
 DATA_SETS = ["cacm", "med", "npl"]
 
-# set variables based on model shortcut
+# set variables based on selected model shortcut
 if (MODEL_SHORTCUT == "ml_miniLM_L12_v2"):
     MODEL = "paraphrase-multilingual-MiniLM-L12-v2"
     EMBEDD_DIMENSION = 384
@@ -18,3 +18,9 @@ elif (MODEL_SHORTCUT == "ml_mpnet_base_v2"):
 elif (MODEL_SHORTCUT == "dbmlc_v1"):
     MODEL = "distiluse-base-multilingual-cased-v1"
     EMBEDD_DIMENSION = 512
+
+# model fine-tuning
+MODEL_PATH = f"./task_04/models/{MODEL_SHORTCUT}"
+EPOCHS = 1
+WARMUP_STEPS = 500  # ? fraction of data set size
+BATCH_SIZE = 16

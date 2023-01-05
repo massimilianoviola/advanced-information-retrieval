9f61d61b32a1a469323b202a9a3c91940b74f202
mriedl <manuel.riedl@student.tugraz.at>
2023-01-05 01:27:17 +0100

    Search files of med and cacm

--- task_01/search.py
+++ task_01/search.py
@@ -1,61 +1,40 @@
+import argparse
 import json
-import os
 from elasticsearch import Elasticsearch
 import constants
 
-if __name__ == '__main__':
+# connect to the server
+es = Elasticsearch("http://localhost:9200")
 
-    es = Elasticsearch("http://localhost:9200")
+filename = "/home/manuel/PycharmProjects/advanced-information-retrieval/data/" + constants.QUERY_FILE + ".json"
+# load queries in JSON format
+with open(filename, "r") as infile:
+    queries = json.loads(infile.read())
+    queries_list = queries["QUERIES"]
 
-    query_template = {
-        "script_score": {
-            "query": {
-                "match_all": {}
-            },
-            # https://www.elastic.co/guide/en/elasticsearch/reference/current/modules-scripting-using.html
-            "script": {
-                "source":  # "cosineSimilarity(params.query_embedd, 'EMBEDD') + 1.0",  # ! does this work?
-                # dotProduct is the same as cosineSimilarity on normalized vectors
-                    "dotProduct(params.query_embedd, 'EMBEDD') + 1.0",
-                "params": {
-                    "query_embedd": None
+for index in constants.INDEX_NAMES_TO_SEARCH:
+    output_filename = f"/home/manuel/PycharmProjects/advanced-information-retrieval/task_01/outputs/{index}_bm25"
+    with open(output_filename, "w") as output:
+        for query in queries_list:
+            num = query["QUERYID"]
+            text = query["QUERY"]
+            query_dict = {
+                "bool": {
+                    "should": [
+                        { "match": { "TEXT": text } }
+                    ]
                 }
             }
-        }
-    }
-
-    query_params = query_template["script_score"]["script"]["params"]
-
-    # execute search for each data set and language
-
-    for data_set in constants.DATA_SETS:
-            index_name = f"{data_set}".lower()
-
-            queries_file = f"/home/manuel/PycharmProjects/advanced-information-retrieval/data/{data_set}/queries.json"
-            outputs_file = f"/home/manuel/PycharmProjects/advanced-information-retrieval/task_01/outputs/{data_set}_summarize.txt"
-
-            with open(queries_file, "r") as queries, open(outputs_file, "w") as outputs:
-                for query in json.loads(queries.read())["QUERIES"]:
-                    query_params["query_embedd"] = query["QUERY"]
-                    response = es.search(index=index_name, query=query_template,
-                                         size=1000)  # return 1000 most relevant documents
-                    query_id = query["QUERYID"]
-                    for rank, hit in enumerate(response["hits"]["hits"]):
-                        # input format: query_id Q0 doc_id rank_of_document score_of_document tag_for_current_run
-                        print(query_id,
-                              "Q0",
-                              hit["_source"]["DOCID"],
-                              rank + 1,
-                              hit["_score"],
-                              "tag",
-                              sep="\t",
-                              file=outputs)
-
-    # evaluate results with trec_eval
-    for data_set in constants.DATA_SETS:
-            # calculate Mean Average Precision (MAP) using existing relevancies (qrels-treceval.txt) and search results (outputs)
-            # store in results: map queryID score
-            os.system(
-                f"trec_eval -m map -q /home/manuel/PycharmProjects/advanced-information-retrieval/data/{data_set}/qrels-treceval.txt /home/manuel/PycharmProjects/advanced-information-retrieval/task_01/outputs/{data_set}_summarize.txt > /home/manuel/PycharmProjects/advanced-information-retrieval/task_01/results/{data_set}_summarize.txt"
+            response = es.search(
+                index=index,
+                query=query_dict,
+                size=1000
             )
+
+            rank = 0
+            for hit in response["hits"]["hits"]:
+                rank += 1
+                docid = hit["_source"]["DOCID"]
+                score = hit["_score"]
+                print(num, "Q0", docid, rank, score, "tag", sep="\t",file=output)
 

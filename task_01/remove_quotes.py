8b3c4c19ca3d030f97364126373d4c0c7b7cc805
mriedl <manuel.riedl@student.tugraz.at>
2023-01-07 23:18:10 +0100

    Added remove_quotes.py and plots for multiple summarizations

---
+++ task_01/remove_quotes.py
@@ -0,0 +1,21 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("datafile", type=str, help="Input file")
+args = parser.parse_args()
+
+filename = args.datafile
+
+parsed_lines = []
+with open(filename, 'r') as in_f:
+    # Iterate over each line in the input file
+    for line in in_f:
+        start_idx = line.index('"TEXT": "') + len('"TEXT": "')
+        end_idx = line.index('"}', start_idx)
+        text = line[start_idx:end_idx].replace('"', ' ')
+        line = line[:start_idx] + text + line[end_idx:]
+        parsed_lines.append(line)
+
+with open(filename,'w') as out_file:
+    for line in parsed_lines:
+        out_file.write(line)
--- task_01/run_task.py
+++ task_01/run_task.py
@@ -14,21 +14,26 @@ for data in DATA_SETS:
     os.system(f"python3 summarize_data.py {data}")
     print()
 print("----------------------------------------------------------")
+print("Remove quotes to bring file in json format")
+print("----------------------------------------------------------")
+for data in DATA_SETS:
+    os.system(f"python3 remove_quotes.py ../data/{data}/{data}_summarize_en.json")
+print("----------------------------------------------------------")
 print("Index datasets in elasticsearch")
 print("----------------------------------------------------------")
 for data in DATA_SETS:
-    os.system(f"python3 ../bm25_index.py ../data/{data}/{data}.json {data}")
+    os.system(f"python3 ../bm25_index.py -f ../data/{data}/{data}.json -i {data}")
     print()
-    os.system(f"python3 ../bm25_index.py ../data/{data}/{data}_summarize_en.json {data}_summarize_en")
+    os.system(f"python3 ../bm25_index.py -f ../data/{data}/{data}_summarize_en.json -i {data}_summarize_en")
     print()
 print("----------------------------------------------------------")
 print("Search using bm25")
 print("----------------------------------------------------------")\
 
 for data in DATA_SETS:
-    os.system(f"python3 ../bm25_search.py ../data/{data}/queries.json {data} > ./outputs/{data}_bm25.txt")
+    os.system(f"python3 ../bm25_search.py -q ../data/{data}/queries.json -i {data} > ./outputs/{data}_bm25.txt")
     print(f"Search for index {data} is done... ")
-    os.system(f"python3 ../bm25_search.py ../data/{data}/queries.json {data}_summarize_en > ./outputs/{data}_summarize_en_bm25.txt")
+    os.system(f"python3 ../bm25_search.py -q ../data/{data}/queries.json -i {data}_summarize_en > ./outputs/{data}_summarize_en_bm25.txt")
     print(f"Search for index {data}_summarize_en is done... ")
 
 print("----------------------------------------------------------")
@@ -38,7 +43,7 @@ for data in DATA_SETS:
     os.system(f"trec_eval -m map -q ../data/{data}/qrels-treceval.txt ./outputs/{data}_bm25.txt > ./results/{data}_bm25.txt")
     print(f"Trec eval  {data} is done... ")
     os.system(f"trec_eval -m map -q ../data/{data}/qrels-treceval.txt ./outputs/{data}_summarize_en_bm25.txt > ./results/{data}_summarize_en_bm25.txt")
-    print(f"Trec eval  {data} is done... ")
+    print(f"Trec eval  {data}_summarize is done... ")
 print("----------------------------------------------------------")
 print("Task 01 is done")
 print("----------------------------------------------------------")

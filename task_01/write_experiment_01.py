6a0aa50cc483af9cd53ebc95742df6d0b8bb07ee
David Mihola <mihola.dejv@gmail.com>
2023-01-08 12:12:42 +0100

    shell-reorder of tasks 01 and 02, removal of presentation notebook

--- task_01/write_experiment_01.py
+++ task_01/write_experiment_01.py
@@ -1,34 +1,47 @@
 import functools
 import os
+import sys
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--file", action='store_true')
+args = parser.parse_args()
 
 output_path = "task_01/outputs" # save search script outputs
 result_path = "task_01/results" # save analysis made by trec_eval
 datasets = ["med", "cacm", "npl"]
+models = ["average_word_embeddings_glove.6B.300d", "all-MiniLM-L6-v2", "all-MiniLM-L12-v2", "all-mpnet-base-v2"]
+shortcuts = ["glove", "minil6", "minil12", "mpnetv2"]
 
-# write the experiment shell script to be executed via the command line
-with open("task_01/experiment_01.sh", "w") as f:
-    printf = functools.partial(print, file=f)
+file = open("task_01/experiment_01.sh", "w") if args.file else sys.stdout
 
-    printf("# experiment 01: compare retrieval performance on summarized documents")
-    printf(f"mkdir -p {output_path}")
-    printf(f"mkdir -p {result_path}")
+# write the experiment in shell language to be executed via the command line
+printf = functools.partial(print, file=file)
 
-    for d in datasets:
-        printf(f"# index {d}")
-        printf(f"python3 bm25_index.py ./data/{d}/{d}.json {d}_bm25")
+printf("# experiment 01: compare retrieval performance with language models")
+printf(f"mkdir -p {output_path}")
+printf(f"mkdir -p {result_path}")
 
-        printf(f"# index summarized {d}")
-        printf(f"python3 bm25_index.py ./data/{d}/summarized_{d}.json summarized_{d}_bm25")
+for d in datasets:
+    printf(f"# index {d}")
+    printf(f"python3 bm25_index.py ./data/{d}/{d}.json {d}_bm25")
 
-        printf(f"# search {d}")
-        printf(f"python3 bm25_search.py ./data/{d}/queries.json {d}_bm25 > {os.path.join(output_path, f'{d}_bm25.txt')}")
+    for m, s in zip(models, shortcuts):
+        printf(f"# index {d} with model {m}")
+        printf(f"python3 embedding_index.py ./data/{d}/{d}.json {d}_{s} {m}")
 
-        printf(f"# search summarized {d}")
-        printf(f"python3 bm25_search.py ./data/{d}/queries.json summarized_{d}_bm25 > {os.path.join(output_path, f'summarized_{d}_bm25.txt')}")
+    printf(f"# search {d}")
+    printf(f"python3 bm25_search.py ./data/{d}/queries.json {d}_bm25 > {os.path.join(output_path, f'{d}_bm25.txt')}")
 
-        printf(f"# analyze {d}")
-        printf(f"./trec_eval -m map -q ./data/{d}/qrels-treceval.txt {os.path.join(output_path, f'{d}_bm25.txt')} > {os.path.join(result_path, f'map_{d}_bm25.txt')}")
+    for m, s in zip(models, shortcuts):
+        printf(f"# search {d} with model {m}")
+        printf(f"python3 embedding_search.py ./data/{d}/queries.json {d}_{s} {m} > {os.path.join(output_path, f'{d}_{s}.txt')}")
 
-        printf(f"# analyze summarized {d}")
-        printf(f"./trec_eval -m map -q ./data/{d}/qrels-treceval.txt {os.path.join(output_path, f'summarized_{d}_bm25.txt')} > {os.path.join(result_path, f'map_summarized_{d}_bm25.txt')}")
+    printf(f"# analyze {d}")
+    printf(f"./trec_eval -m map -q ./data/{d}/qrels-treceval.txt {os.path.join(output_path, f'{d}_bm25.txt')} > {os.path.join(result_path, f'map_{d}_bm25.txt')}")
+
+    for m, s in zip(models, shortcuts):
+        printf(f"# analyze {d} with model {m}")
+        printf(f"./trec_eval -m map -q ./data/{d}/qrels-treceval.txt {os.path.join(output_path, f'{d}_{s}.txt')} > {os.path.join(result_path, f'map_{d}_{s}.txt')}")
+
+file.close()
